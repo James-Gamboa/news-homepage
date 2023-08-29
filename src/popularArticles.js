2ddcc10dfacb63a20e5ef787a3aba29ca8114f5f
James <jjguevarag@gmail.com>
2023-08-29 12:06:00 -0600

    complete my project

---
+++ src/popularArticles.js
@@ -0,0 +1,32 @@
+// @ts-nocheck
+document.querySelector('#popularArticles').innerHTML = `
+<section id="topPopular">
+<img class="imgRetro" src="../public/img/image-retro-pcs.jpg" alt="">
+<div class="popularInfo">
+<h4>01</h4>
+<h5><a href="#">Reviving Retro PCs</a></h5>
+<p>What happens when old PCs are given modern upgrades?</p>
+</div>
+
+</section>
+<section id="topPopular">
+<img class="imgRetro" src="../public/img/image-top-laptops.jpg" alt="">
+<div class="popularInfo">
+<h4>02</h4>
+<h5><a href="#">Top 10 Laptops of 2022</a></h5>
+<p>Our best picks for various needs and budgets. </p>
+</div>
+
+</section>
+<section id="topPopular">
+<img class="imgRetro" src="../public/img/image-gaming-growth.jpg" alt="">
+<div class="popularInfo">
+<h4>03</h4>
+<h5><a href="#">The Growth of Gaming</a></h5>
+<p>How the pandemic has sparked fresh opportunities.</p>
+</div>
+
+</section>
+
+
+`
